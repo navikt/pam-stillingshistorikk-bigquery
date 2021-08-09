609f773348ec41a9831d981d6d20279b03fb5fe0
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-08-09 11:08:13 +0200

    added status controller

---
+++ src/main/kotlin/no/nav/arbeidsplassen/stihibi/KafkaStateRegistry.kt
@@ -0,0 +1,28 @@
+package no.nav.arbeidsplassen.stihibi
+
+import javax.inject.Singleton
+
+@Singleton
+class KafkaStateRegistry {
+
+    private val stateRegistry = hashMapOf<String, KafkaState>()
+
+    fun setConsumerToError(consumer:String) {
+        stateRegistry[consumer] = KafkaState.ERROR
+    }
+
+    fun setConsumerToPaused(consumer: String) {
+        stateRegistry[consumer] = KafkaState.PAUSED
+    }
+
+    fun hasError(): Boolean {
+       return (stateRegistry.isNotEmpty() && stateRegistry.values.contains(KafkaState.ERROR))
+    }
+
+}
+
+enum class KafkaState {
+    RUNNING, PAUSED, ERROR
+}
+
+
---
+++ src/main/kotlin/no/nav/arbeidsplassen/stihibi/StatusController.kt
@@ -0,0 +1,40 @@
+package no.nav.arbeidsplassen.stihibi
+
+import io.micronaut.configuration.kafka.ConsumerRegistry
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import org.slf4j.LoggerFactory
+
+@Controller("/internal")
+class StatusController(private val kafkaStateRegistry: KafkaStateRegistry, private val consumerRegistry: ConsumerRegistry) {
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(StatusController::class.java)
+    }
+
+    @Get("/isReady")
+    fun isReady(): String {
+        return "OK"
+    }
+
+    @Get("/isAlive")
+    fun isAlive(): HttpResponse<String> {
+        if (kafkaStateRegistry.hasError()) {
+            LOG.error("A Kafka consumer is set to Error, setting all consumers to pause")
+            consumerRegistry.consumerIds
+                .forEach {
+                    LOG.error("Pausing consumer $it")
+                    consumerRegistry.pause(it)
+                }
+            return HttpResponse.serverError("Kafka consumer is not running")
+        }
+        return HttpResponse.ok("OK")
+    }
+
+    @Get("/kafkaState")
+    fun setKafkaState(): Boolean {
+        return kafkaStateRegistry.hasError()
+    }
+
+}
