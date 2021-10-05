3dbf20f27b8b9a90ecef98ed49ff5afcca9794f5
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2021-10-05 08:34:59 +0200

    changed enpoinds

--- src/main/kotlin/no/nav/arbeidsplassen/stihibi/api/v1/AdministrationTimeController.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/stihibi/api/v1/AdministrationTimeController.kt
@@ -8,14 +8,14 @@ import org.slf4j.LoggerFactory
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-@Controller("/api/v1/admin/time")
+@Controller("/api/v1/admin/report")
 class AdministrationTimeController(private val bigQueryService: BigQueryService) {
 
     companion object {
         private val LOG = LoggerFactory.getLogger(AdministrationTimeController::class.java)
     }
 
-    @Get("/report.csv", produces = ["text/csv"] )
+    @Get("/behandlingstid.csv", produces = ["text/csv"] )
     fun administrationTime(@QueryValue from: String?, @QueryValue to: String?):String {
         val periodFrom = if (from!=null) LocalDate.parse(from, DateTimeFormatter.ISO_DATE) else LocalDate.now().withDayOfMonth( 1 )
         val periodTo = if (to!=null) LocalDate.parse(to, DateTimeFormatter.ISO_DATE) else periodFrom.plusMonths(1)
