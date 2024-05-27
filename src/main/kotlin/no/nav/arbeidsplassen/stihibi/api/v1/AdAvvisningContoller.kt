094c8ccbb8c9c51ea89b9d071aab7958664c39f5
Oscar Vladau-Husevold <oscarnvh@gmail.com>
2024-05-27 13:32:55 +0200

    Legger til en error handler i AdAvvisningController

--- src/main/kotlin/no/nav/arbeidsplassen/stihibi/api/v1/AdAvvisningContoller.kt
+++ src/main/kotlin/no/nav/arbeidsplassen/stihibi/api/v1/AdAvvisningContoller.kt
@@ -2,6 +2,9 @@ package no.nav.arbeidsplassen.stihibi.api.v1
 
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Error
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.HttpResponse
 import no.nav.arbeidsplassen.stihibi.Avvisning
 import no.nav.arbeidsplassen.stihibi.BigQueryService
 import org.slf4j.LoggerFactory
@@ -16,5 +19,11 @@ class AdAvvisningContoller(private val bigQueryService: BigQueryService) {
     fun retrieveAdHistory(): List<Avvisning> {
         LOG.info("Henter alle avvisninger det siste året")
         return bigQueryService.queryAvvisning()
+    }
+
+    @Error
+    fun handleError(request: HttpRequest<*>, e: Exception): HttpResponse<String> {
+        LOG.error("Error handling request ${request.uri}", e)
+        return HttpResponse.serverError("An error occurred while processing your request.")
     }
 }
