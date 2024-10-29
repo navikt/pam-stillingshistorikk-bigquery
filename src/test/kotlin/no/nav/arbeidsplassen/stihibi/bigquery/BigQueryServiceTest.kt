f839c6027fe52c4e1f8c65d8a75c4d62a11e4cfe
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2024-10-29 15:06:09 +0100

    renser opp i testdata og retter opp tester

--- src/test/kotlin/no/nav/arbeidsplassen/stihibi/bigquery/BigQueryServiceTest.kt
+++ src/test/kotlin/no/nav/arbeidsplassen/stihibi/bigquery/BigQueryServiceTest.kt
@@ -1,7 +1,5 @@
 package no.nav.arbeidsplassen.stihibi.bigquery
 
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.arbeidsplassen.stihibi.AdTransport
 import no.nav.arbeidsplassen.stihibi.BigQueryService
 import no.nav.arbeidsplassen.stihibi.bigquery.app.test.TestRunningApplication
@@ -14,23 +12,20 @@ import org.junit.jupiter.api.TestInstance
 class BigQueryServiceTest : TestRunningApplication() {
 
     private val bigQueryService = BigQueryService(appCtx.adSchemaTableDefinition, appCtx.bigQuery, appCtx.objectMapper)
-    private val testData = javaClass.getResourceAsStream("/stihibi-dummy-data.json")
-    private val stillingerJSON = appCtx.objectMapper.readValue(testData, JsonNode::class.java)
+    private val stillinger: List<AdTransport> = appCtx.objectMapper.readValue(
+        javaClass.getResourceAsStream("/stihibi-dummy-data.json"),
+        appCtx.objectMapper.typeFactory.constructCollectionType(List::class.java, AdTransport::class.java)
+    )
 
     @Test
     fun `Skal sende stillinger til BigQuery`() {
-        val stillinger = ArrayList<AdTransport>()
-        stillingerJSON.map { rad ->
-            stillinger.add(appCtx.objectMapper.readValue(rad.get("json").asText(), AdTransport::class.java))
-        }
-
         val offsets = MutableList<Long>(stillinger.size) { 0 }
         val partitions = MutableList<Int>(stillinger.size) { 0 }
         val topics = MutableList<String>(stillinger.size) { "" }
 
         val response = bigQueryService.sendBatch(stillinger, offsets, partitions, topics)
 
-        assertThat(stillinger.size).isEqualTo(20)
+        assertThat(stillinger.size).isEqualTo(3)
         assertThat(response.hasError).isFalse()
         assertThat(response.rowsError).isEqualTo(0)
     }
@@ -38,14 +33,15 @@ class BigQueryServiceTest : TestRunningApplication() {
     @Test
     fun `Skal hente alle avviste stillinger siste året`() {
         val response = bigQueryService.queryAvvisning()
-        assertThat(response.size).isEqualTo(10)
+        assertThat(response.size).isEqualTo(3)
         assertThat(response.first().reportee).isEqualTo("Marve Almar Fleksnes")
     }
 
     @Test
     fun `Skal hente historikken til en stilling`() {
-        val forventetStilling: AdTransport = appCtx.objectMapper.readValue(stillingerJSON[0].get("json").asText())
-        val response = bigQueryService.queryAdHistory(uuid = forventetStilling.uuid, year = forventetStilling.created.year)
+        val forventetStilling: AdTransport = stillinger[0]
+        val response =
+            bigQueryService.queryAdHistory(uuid = forventetStilling.uuid, year = forventetStilling.created.year)
 
         assertThat(response.size).isEqualTo(1)
         assertThat(response.first()).isEqualTo(forventetStilling)
